39c8e1ba92a5a03a69a81758c71559a007c37301
ringcrl <ringcrl@foxmail.com>
2020-06-06 12:30:44 +0800

    feat(build): 配置 TS 支持

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,15 @@
 module.exports = {
-  "parser": "@typescript-eslint/parser",
-  "plugins": ["@typescript-eslint"],
-  "extends": [
-    "airbnb-base",
-    "plugin:@typescript-eslint/recommended"
-  ]
-}
+  env: {
+    browser: true,
+    node: true,
+  },
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: ["airbnb-base", "airbnb-typescript/base"],
+  parserOptions: {
+    project: "./tsconfig.json",
+    // https://github.com/typescript-eslint/typescript-eslint/issues/967
+    createDefaultProgram: true,
+  },
+  rules: {},
+};
