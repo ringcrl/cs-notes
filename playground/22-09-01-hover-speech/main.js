96ef72a98633e70b5c30d719b1061f3da0d463b4
chenng <ringcrl@foxmail.com>
2022-09-01 07:28:18 +0800

    feat: update

--- playground/22-09-01-hover-speech/main.js
+++ playground/22-09-01-hover-speech/main.js
@@ -13,12 +13,19 @@ const extractText = (DOMElement) => [...DOMElement.childNodes] // has childNodes
 
 function move (e) {
   const el = document.elementFromPoint(e.clientX, e.clientY)
-  const text = extractText(el)[0]
-  console.log(text)
-  const utterance = new window.SpeechSynthesisUtterance(text)
+  const texts = extractText(el)
+  console.log(texts)
+  const text = texts.join()
+  if (!text) return
+  const utterance = new window.SpeechSynthesisUtterance()
+  utterance.text = text // 要合成的文本
+  // utterance.lang = 'en-US' // 美式英语发音（默认自动选择）
+  utterance.rate = 1.6 // 二倍速（默认为 1，范围 0.1～10）
+  // utterance.pitch = 2 // 高音调（数字越大越尖锐，默认为 1，范围 0～2 ）
+  // utterance.volume = 0.5 // 音量 0.5 倍（默认为1，范围 0～1）
   window.speechSynthesis.speak(utterance)
 }
 
 const debounceMove = debounce(move, 1000)
 
-document.onmousemove = debounceMove
+document.addEventListener('mousemove', debounceMove)
