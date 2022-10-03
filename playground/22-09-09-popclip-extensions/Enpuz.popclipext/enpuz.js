d21faa4b849bf4cf03d8fda8436dfb147f6745d8
chenng <ringcrl@foxmail.com>
2022-10-04 07:06:08 +0800

    feat: update

--- playground/22-09-09-popclip-extensions/Enpuz.popclipext/enpuz.js
+++ playground/22-09-09-popclip-extensions/Enpuz.popclipext/enpuz.js
@@ -1,3 +1,5 @@
-const sentence = popclip.input.text.split(' ').join('-')
+const sentence = popclip.input.text
+  .replace(/ /g, '-')
+  .replace(/,/g, '+')
 // popclip.pasteText(sentence)
 popclip.openUrl(`http://enpuz.com/${sentence}=`)
