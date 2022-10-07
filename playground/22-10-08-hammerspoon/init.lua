bd7da14666252d5f9bc2089c0c69cbf39e443f1d
chenng <ringcrl@foxmail.com>
2022-10-08 07:50:40 +0800

    feat: update

---
+++ playground/22-10-08-hammerspoon/init.lua
@@ -0,0 +1,50 @@
+-- 滴答清单移动到底部对齐
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "return", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+
+  f.y = f.y + 1286
+  win:setFrame(f)
+end)
+
+-- 左半边
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Left", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+-- 右半边
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Right", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x + (max.w / 2)
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+-- 全屏
+hs.hotkey.bind({"alt", "ctrl"}, "return", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = 0
+  f.y = 0
+  f.w = max.w
+  f.h = max.h
+  win:setFrame(f)
+end)
