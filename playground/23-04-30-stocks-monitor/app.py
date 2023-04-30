1521d6c7738c7e106782f6fa6eb02be7ac257881
chenng <ringcrl@foxmail.com>
2023-04-30 14:54:37 +0800

    feat: update

--- playground/23-04-30-stocks-monitor/app.py
+++ playground/23-04-30-stocks-monitor/app.py
@@ -13,7 +13,9 @@ load_dotenv()
 DISCORD_WEBHOOK_URL = os.environ.get("DISCORD_WEBHOOK_URL")
 POLYGON_API = os.environ.get("POLYGON_API")
 
-stock_list = ['SOXS']
+stock_list = ['SOXS', 'TSLQ', 'AMAGN', 'FNGD']
+
+# region 指标
 
 
 def ema(prices, period):
@@ -63,43 +65,8 @@ def ta_min(prices, period):
     return min_series
 
 
-def send_to_discord(webhook_url, content):
-    data = {
-        "content": content
-    }
-
-    response = requests.post(webhook_url, json=data)
-
-    if response.status_code == 204:
-        print("Message sent successfully!")
-    else:
-        print(f"Error sending message: {response.status_code} {response.text}")
-
-
-def get_ticker_data(name):
-    # 获取当前日期
-    today = datetime.today()
-
-    # 获取明天的日期
-    tomorrow = today + timedelta(days=1)
-    # 获取 10 天前的日期
-    days_ago = today - timedelta(days=5)
-    # 将日期格式化为 '2023-04-28' 形式
-    formatted_tomorrow = tomorrow.strftime("%Y-%m-%d")
-    formatted_ten_days_ago = days_ago.strftime("%Y-%m-%d")
-
-    # https://polygon.io/docs/stocks/get_v2_aggs_ticker__stocksticker__range__multiplier___timespan___from___to
-    response = requests.get(
-        f'https://api.polygon.io/v2/aggs/ticker/{name}/range/1/hour/{formatted_ten_days_ago}/{formatted_tomorrow}?apiKey={POLYGON_API}').text
-
-    return response
-
-
 def smi(data):
-    parsed_data = json.loads(data)
-
-    ticker = parsed_data['ticker']
-    results = parsed_data['results']
+    results = data
     latest_timestamp_ms = results[len(results)-1]['t']
     timestamp_s = latest_timestamp_ms / 1000
     dt = datetime.fromtimestamp(timestamp_s)
@@ -119,32 +86,127 @@ def smi(data):
     SMI_AVGREL = ema(ema(SMI_RDIFF, SMI_B), SMI_B)
     SMI = SMI_AVGREL/(SMI_AVG_DIFF/2)*100
     SMI_AVG = ema(SMI, SMI_B)
+    return SMI, SMI_AVG, formatted_dt
+
+# endregion
+
+# region discord 通知
+
+
+def send_to_discord(webhook_url, content):
+    data = {
+        "content": content
+    }
+
+    response = requests.post(webhook_url, json=data)
+
+    if response.status_code == 204:
+        print("Message sent successfully!")
+    else:
+        print(f"Error sending message: {response.status_code} {response.text}")
+
+# endregion
+
+# region 获取数据
+
+
+def get_ticker_data(name):
+    # 获取当前日期
+    today = datetime.today()
+
+    # 获取明天的日期
+    tomorrow = today + timedelta(days=1)
+    # 获取 10 天前的日期
+    days_ago = today - timedelta(days=5)
+    # 将日期格式化为 '2023-04-28' 形式
+    formatted_tomorrow = tomorrow.strftime("%Y-%m-%d")
+    formatted_ten_days_ago = days_ago.strftime("%Y-%m-%d")
+
+    # https://polygon.io/docs/stocks/get_v2_aggs_ticker__stocksticker__range__multiplier___timespan___from___to
+    response = requests.get(
+        f'https://api.polygon.io/v2/aggs/ticker/{name}/range/1/hour/{formatted_ten_days_ago}/{formatted_tomorrow}?apiKey={POLYGON_API}').text
+
+    print('response', response)
+
+    return response
+
+# endregion
+
+# region 处理数据，获取推送材料
+
+
+def get_status(val):
+    VAL1 = 30
+    VAL2 = 70
+    T1 = VAL1
+    T2 = VAL2
+    B1 = -VAL1
+    B2 = -VAL2
+
+    if val > T2:
+        return 'T2'
+    elif val < T2 and val > T1:
+        return 'T1'
+    elif val < B1 and val > B2:
+        return 'B1'
+    elif val < B2:
+        return 'B2'
+    else:
+        return ''
+
+
+def handle_ticker_data(data):
+    parsed_data = json.loads(data)
+
+    ticker = parsed_data.get('ticker', None)
+    results = parsed_data.get('results', None)
+
+    if results is None:
+        return None
+
+    SMI, SMI_AVG, formatted_dt = smi(results)
 
     LATEST_SMI = SMI[len(SMI)-1]
     LATEST_SMI_AVG = SMI_AVG[len(SMI_AVG)-1]
+
+    status = get_status(LATEST_SMI)
 
     res = f"""
 ticker: {ticker}
 SMI蓝线: {LATEST_SMI}
 SMI红线: {LATEST_SMI_AVG}
+状态: {status}
 日期: {formatted_dt}
 """
 
     return res
 
+# endregion
+
+# region 主程序
+
 
 def stock_monitor():
     for stock in stock_list:
-        ticker_info = get_ticker_data(stock)
-        smi_res = smi(ticker_info)
-        print(smi_res)
-        send_to_discord(DISCORD_WEBHOOK_URL, smi_res)
+        ticker_data = get_ticker_data(stock)
+        ticker_res = handle_ticker_data(ticker_data)
+
+        print('ticker_res', ticker_res)
+
+        if ticker_res is None:
+            continue
+
+        send_to_discord(DISCORD_WEBHOOK_URL, ticker_res)
 
 
 stock_monitor()
 
-# 定时任务
+# endregion
+
+# region 定时任务
 schedule.every().hour.at(':00').do(stock_monitor)
 while True:
     schedule.run_pending()
     time.sleep(60)  # 等待 60 秒再次检查
+
+# endregion
