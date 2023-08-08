321016b7e7613a9635d192c6c86720f0b45a74b1
ringcrl <ringcrl@foxmail.com>
2023-08-08 18:04:18 +0800

    feat: update

---
+++ playground/23-07-13-openai/10-openai-api/shell.sh
@@ -0,0 +1,9 @@
+curl https://api.openai.com/v1/chat/completions \
+  -H "Content-Type: application/json" \
+  -H "Authorization: Bearer openai_key" \
+  -d '{
+     "model": "gpt-3.5-turbo",
+     "messages": [{"role": "user", "content": "Say this is a test!"}],
+     "temperature": 0.7
+   }'
+   
