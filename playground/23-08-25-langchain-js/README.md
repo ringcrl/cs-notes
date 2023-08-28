851c90b68c101d2bea144548ad7e877294a7ec94
ringcrl <ringcrl@foxmail.com>
2023-08-28 19:19:18 +0800

    feat: update

--- playground/23-08-25-langchain-js/README.md
+++ playground/23-08-25-langchain-js/README.md
@@ -1,10 +1,152 @@
+## 调试
+
 ```sh
 # 使用 NodeJS 18
 nvm use 18
 
-# 导出环境变量
-export OPENAI_API_KEY=
+# 参照 .env.example 配置 .env
 
 # 运行
-node main.mjs
+ts-node main.ts
+```
+
+```sh
+# 向量数据库：https://cloud.tencent.com/document/product/1709/95102
+
+# 创建数据库
+curl -i -X POST \
+  -H 'Content-Type: application/json' \
+  -H 'Authorization: Bearer account=root&api_key=eC4bLRy2va******************************' \
+  http://10.0.X.X:80/database/create \
+  -d '{
+        "database": "db_test"
+  }'
+
+# 查询数据库
+curl -i -X GET \
+   -H 'Content-Type: application/json' \
+   -H 'Authorization: Bearer account=root&api_key=A5VOgsMpGWJhUI0WmUbY********************' \
+   http://10.0.X.X:80/database/list
+
+# 创建 Collection
+curl -i -X POST \
+  -H 'Content-Type: application/json' \
+  -H 'Authorization: Bearer account=root&api_key=A5VOgsMpGWJhUI0WmUbY********************' \
+  http://10.0.X.X:80/collection/create \
+  -d '{
+    "database": "db_test",
+    "collection": "collection_test",
+    "replicaNum": 2,
+    "shardNum": 1,
+    "description": "This is the test data of vectordb.",
+    "indexes": [
+      {
+        "fieldName": "id",
+        "fieldType": "string",
+        "indexType": "primaryKey"
+      },
+      {
+        "fieldName": "vector",
+        "fieldType": "vector",
+        "indexType": "HNSW",
+        "dimension": 1536,
+        "metricType": "L2",
+        "params": {
+          "M": 64,
+          "efConstruction": 8
+        }
+      },
+      {
+        "fieldName": "doc_id",
+        "fieldType": "uint64",
+        "indexType": "filter"
+      },
+      {
+        "fieldName": "content",
+        "fieldType": "string",
+        "indexType": "filter"
+      }
+    ]
+  }'
+
+# 查询 Collection
+curl -i -X POST \
+  -H 'Content-Type: application/json' \
+  -H 'Authorization: Bearer account=root&api_key=A5VOgsMpGWJhUI0WmUbY********************' \
+  http://10.0.X.X:80/collection/list \
+  -d '{
+    "database": "db_test"
+  }'
+
+# 插入数据
+curl -i -X POST \
+  -H 'Content-Type: application/json' \
+  -H 'Authorization: Bearer account=root&api_key=A5VOgsMpGWJhUI0WmUbY********************' \
+  http://10.0.X.X:80/document/upsert \
+  -d '{
+    "database": "db_test",
+    "collection": "collection_test",
+    "buildIndex": true,
+    "documents": [
+      {
+        "id": "1",
+        "vector": [0.14,0.25,0.72,0.53,0.67,0.87,0.23,0.78,0.99,0.13],
+        "doc_id": 1,
+        "content": "doc_id=1 content=string_1"
+      },
+      {
+        "id": "2",
+        "vector": [0.23,0.29,0.78,0.81,0.23,0.42,0.82,0.54,0.22,0.80],
+        "doc_id": 1,
+        "content": "doc_id=1 content=string_2"
+      },
+      {
+        "id": "3",
+        "vector": [0.14,0.32,0.88,0.12,0.52,0.32,0.91,0.23,0.23,0.67],
+        "doc_id": 1,
+        "content": "doc_id=1 content=string_3"
+      },
+      {
+        "id": "4",
+        "vector": [0.98,0.52,0.62,0.91,0.99,0.34,0.55,0.35,0.43,0.64],
+        "doc_id": 2,
+        "content": "doc_id=2 content=string_4"
+      },
+      {
+        "id": "5",
+        "vector": [0.67,0.14,0.92,0.45,0.83,0.74,0.23,0.53,0.23,0.55],
+        "doc_id": 2,
+        "content": "doc_id=2 content=string_5"
+      }
+    ]
+  }'
+
+# 向量检索数据
+curl -i -X POST \
+  -H 'Content-Type: application/json' \
+  -H 'Authorization: Bearer account=root&api_key=A5VOgsMpGWJhUI0WmUbY********************' \
+  http://10.0.X.X:80/document/search \
+  -d '{
+    "database": "db_test",
+    "collection": "collection_test",
+    "search": {
+      "vectors": [
+        [0.2,0.14,0.23,0.5,0.65,0.34,0.89,0.64,0.23,0.22]
+      ],
+      "retrieveVector": false,
+      "limit": 2,
+      "filter": "doc_id=1"
+    }
+  }'
+
+# 删除 Collection
+curl -i -X POST \
+  -H 'Content-Type: application/json' \
+  -H 'Authorization: Bearer account=root&api_key=A5VOgsMpGWJhUI0WmUbY********************' \
+  http://10.0.X.X:80/collection/drop \
+ -d '{
+   "database": "db_test",
+   "collection": "collection_test"
+}'
+
 ```
--- playground/23-08-25-langchain-js/main.ts
+++ playground/23-08-25-langchain-js/main.ts
@@ -1,34 +1,133 @@
-import * as fs from 'fs'
 import * as path from 'path'
 
-import { MemoryVectorStore } from 'langchain/vectorstores/memory'
 import { OpenAIEmbeddings } from 'langchain/embeddings/openai'
+import { PDFLoader } from 'langchain/document_loaders/fs/pdf'
 import { Document } from 'langchain/document'
 import { CharacterTextSplitter } from 'langchain/text_splitter'
 import * as dotenv from 'dotenv'
+import axios from 'axios'
+import { v4 as uuid } from 'uuid'
 
-dotenv.config()
+dotenv.config({ path: path.resolve(__dirname, '.env') })
 
-const { OPENAI_API_KEY } = process.env
+const { VECTOR_DB_IP, VECTOR_DB_KEY } = process.env
 
-const readFile = (localPath: string): string => {
-  const file = fs.readFileSync(localPath, 'utf8')
-  return file
+const DOC_ID = 3
+const DATABASE = 'db_test'
+const COLLECTION = 'collection_test'
+
+const HEADERS = {
+  'Content-Type': 'application/json',
+  Authorization: `Bearer account=root&api_key=${VECTOR_DB_KEY as string}`
 }
 
-const getContentSplitedDocs = async (content: string): Promise<Document[]> => {
-  const splitter = new CharacterTextSplitter({
-    separator: ' ',
-    chunkSize: 7,
-    chunkOverlap: 3
+const embeddings = new OpenAIEmbeddings({
+  batchSize: 512
+})
+
+interface IEmbedDoc {
+  vector: number[]
+  content: string
+}
+const embedDocs = async (docs: Document[]): Promise<IEmbedDoc[]> => {
+  const embedRes = await embeddings.embedDocuments(docs.map((doc) => doc.pageContent))
+
+  return embedRes.map((vector: any, index: number) => {
+    return {
+      content: docs[index].pageContent,
+      vector
+    }
   })
-  const output = await splitter.createDocuments([content])
-  return output
+}
+
+const loadPDFFile = async (localPath: string): Promise<any> => {
+  const loader = new PDFLoader(localPath)
+  const docs = await loader.load()
+
+  // return docs
+
+  // 节约 token，测试用
+  return docs.slice(0, 2)
+}
+
+const getDocsCharacterTextSplitRes = async (docs: Document[]): Promise<Document[]> => {
+  const splitter = new CharacterTextSplitter({
+    separator: '\n',
+    chunkSize: 500,
+    chunkOverlap: 50
+  })
+  const res = await splitter.splitDocuments(docs)
+  return res
+}
+
+// 单次最多插入1000条：https://cloud.tencent.com/document/product/1709/95121
+const addDocsToVectorStore = async (docs: IEmbedDoc[]): Promise<any> => {
+  const documents = docs.map((doc) => {
+    return {
+      id: uuid(),
+      content: doc.content,
+      vector: doc.vector,
+      doc_id: DOC_ID
+    }
+  })
+
+  const res = await axios({
+    method: 'post',
+    url: `http://${VECTOR_DB_IP as string}:80/document/upsert`,
+    headers: HEADERS,
+    data: JSON.stringify({
+      database: DATABASE,
+      collection: COLLECTION,
+      buildIndex: true,
+      documents
+    })
+  })
+
+  return res.data
+}
+
+const searchTopKFromVectorStore = async (query: string): Promise<any> => {
+  const queryVector = await embeddings.embedQuery(query)
+
+  const res = await axios({
+    method: 'post',
+    url: `http://${VECTOR_DB_IP as string}:80/document/search`,
+    headers: HEADERS,
+    data: JSON.stringify({
+      database: DATABASE,
+      collection: COLLECTION,
+      search: {
+        vectors: [queryVector],
+        retrieveVector: false,
+        limit: 2,
+        filter: `doc_id=${DOC_ID}`
+      }
+    })
+  })
+
+  return res.data
+}
+
+const add = async (): Promise<any> => {
+  const docsOfPDF = await loadPDFFile(path.resolve(__dirname, './1.pdf'))
+  const characterTextSplittedRes = await getDocsCharacterTextSplitRes(docsOfPDF)
+  const embedDocsRes = await embedDocs(characterTextSplittedRes)
+
+  const addDocsToVectorStoreRes = await addDocsToVectorStore(embedDocsRes)
+  console.log('addDocsToVectorStoreRes', addDocsToVectorStoreRes)
+}
+
+const search = async (): Promise<any> => {
+  const searchTopKFromVectorStoreRes = await searchTopKFromVectorStore('云点播')
+  console.log('searchTopKFromVectorStoreRes', searchTopKFromVectorStoreRes)
 }
 
 async function main (): Promise<void> {
-  const fileContent = readFile(path.resolve(__dirname, './1.txt'))
-  const docs = await getContentSplitedDocs(fileContent)
+  // 构建
+  // await add()
+
+  // 检索
+  await search()
 }
 
 main().catch((err) => { console.error(err) })
